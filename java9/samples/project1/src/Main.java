b5aa98d9c9de9e2253c585c5579f0746f755b09f
Sattar Hummatli <settarxan@gmail.com>
2018-02-13 10:06:41 +0400

    tested lambda expression

--- java9/samples/project1/src/Main.java
+++ java9/samples/project1/src/Main.java
@@ -2,5 +2,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java 9 world");
 
+        //use of lambda ex case 1
+        callLambdaEx(() -> System.out.println("I'm executing from lambda"));
+
+        //use of lambda ex case 2
+        SimpleInterface si = () -> System.out.println("I'm executing from lambda, case 2");
+        si.doSomthing();
+
+    }
+
+    private static void callLambdaEx(SimpleInterface si) {
+        si.doSomthing();
     }
 }
